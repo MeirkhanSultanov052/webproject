1122a9a50d47cc27278acbb56ede0fed066cd57c
MeirkhanSultanov <75573335+MeirkhanSultanov@users.noreply.github.com>
2021-04-19 17:25:05 +0600

    created 3 main pages with css and js files

---
+++ public/page2.js
@@ -0,0 +1,9 @@
+function big(element) {
+	element.style.fontSize = "23px";
+}
+function small(element) {
+	element.style.fontSize = "20px";
+}
+function load() {
+	$(".text").animate({opacity: '1'}, "400");
+}
